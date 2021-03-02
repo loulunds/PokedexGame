def92cee9e0b705f0219c975ceeacdc2747996c6
loulunds <lousanne.lundstrom@gmail.com>
2021-03-02 13:43:21 +0000

    pokedex game logic made, play again button added

---
+++ pokemon/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+// import Pokedex from './Pokedex'
+import Pokegame from './Pokegame';
+
+function App() {
+  return (
+    <div className="App">
+     <Pokegame />
+    </div>
+  );
+}
+
+export default App;
